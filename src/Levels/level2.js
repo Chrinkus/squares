89f3138ecca743a7dd70ef6da5a96639bc3921ab
Chrinkus <seeschickrun@gmail.com>
2017-03-07 14:37:12 -0600

    Added second level layout

---
+++ src/Levels/level2.js
@@ -0,0 +1,37 @@
+var Scene = require("../Constructors/scene");
+
+var level2 = new Scene();
+
+level2.blockSize = 32;
+
+level2.plan = [
+    "################################",
+    "######                    ######",
+    "##        *          *        ##",
+    "#                              #",
+    "#     *    #   **   #    *     #",
+    "#          #        #          #",
+    "#         ##   **   ##         #",
+    "###   ######        ######   ###",
+    "#         ##   **   ##         #",
+    "#          ##      ##          #",
+    "#                              #",
+    "#     *                  *     #",
+    "#                              #",
+    "#   #       ##    ##       #   #",
+    "#  ##   *   #  @   #   *   ##  #",
+    "####        #      #        ####",
+    "###         #      #         ###",
+    "################################"
+];
+
+level2.colors = {
+    background: "coral",
+    wall: "aqua",
+    pellet: "gold",
+    player: "white"
+};
+
+level2.planReader();
+
+module.exports = level2;
